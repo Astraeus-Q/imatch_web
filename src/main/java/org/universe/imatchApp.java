477ce5a754da0eca8bf467e96294517f1d9f645b
Astraeus-Q <1521050540@qq.com>
2024-01-25 02:44:25 +0800

    Finally implement the primitive front end of website. 15%

---
+++ src/main/java/org/universe/imatchApp.java
@@ -0,0 +1,19 @@
+package org.universe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class imatchApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(imatchApp.class, args);
+    }
+
+}
